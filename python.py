dbd9de523611a69a270c09998ea9afec426cbe8e
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2020-05-01 21:43:13 +0100

    if else statement added

---
+++ python.py
@@ -0,0 +1,17 @@
+import random
+
+def toss_coin():
+    coin_toss = random.randit(0, 2)
+
+    if coin_toss == 0;
+    return "heads"
+
+    else;
+    return "tails"
+
+def prompt():
+    user_guess = ""
+
+    while True ;
+
+    return user_guess
