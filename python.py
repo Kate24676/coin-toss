1018712d72c46aa3decc66ebe3833e9645902c80
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2020-05-24 23:30:43 +0100

    prompt function added

--- python.py
+++ python.py
@@ -3,15 +3,20 @@ import random
 def toss_coin():
     coin_toss = random.randit(0, 2)
 
-    if coin_toss == 0;
+    if coin_toss == 0:
     return "heads"
 
-    else;
+    else:
     return "tails"
 
 def prompt():
     user_guess = ""
 
-    while True ;
+    while True:
+        user_guess = input("Please enter 'heads' or 'tails'(withot quotes): ").lower()
+        if user_guess ="heads" pr user_guess = "tails"
+        break
 
     return user_guess
+
+    print(promt())
